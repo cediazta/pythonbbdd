c1ff077093d6a6669dabc5a9faeaf9f11c9e27db
cediazta <cdiazt.1987@gmail.com>
2026-02-12 20:22:54 +0100

    actualizado

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hola mundo")
+from services import service01prueba
+
+print("Soy un Main")
+texto = service01prueba.getSaludo()
+print(texto)
---
+++ services/service01prueba.py
@@ -0,0 +1,3 @@
+# Vamos a tener un simple metodo para llamarlo desde el main
+def getSaludo():
+    return "Hoy es juernes"
